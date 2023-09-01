09c28d340f7ffb4c5c121a84a495a2fb27734f87
shortboy7 <thdwldnd7@naver.com>
2023-09-01 13:42:49 +0900

    feat: add get landmark list service

--- src/main/java/paldo_bottle/backend/region/service/RegionService.java
+++ src/main/java/paldo_bottle/backend/region/service/RegionService.java
@@ -5,9 +5,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import paldo_bottle.backend.DAO.Region;
 import paldo_bottle.backend.DTO.GetLandmarkListRes;
+import paldo_bottle.backend.global.exception.BaseException;
+import paldo_bottle.backend.global.exception.BaseResponseStatus;
 import paldo_bottle.backend.region.repository.RegionRepository;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,8 +20,23 @@ import java.util.List;
 public class RegionService {
     private final RegionRepository regionRepository;
 
-    List<GetLandmarkListRes> getLandmarkList() {
+    List<GetLandmarkListRes> getLandmarkList() throws BaseException{
         List<Region> regions = regionRepository.findAll();
-        
+        Optional<List<GetLandmarkListRes>> result = regions.stream()
+                .map((region) ->
+                        region.getLandmarkList().stream()
+                                .map((landmark) -> new GetLandmarkListRes(
+                                        landmark.getSupDistrict(),
+                                        landmark.getDistrict(),
+                                        landmark.getName(),
+                                        landmark.getLongitude(),
+                                        landmark.getLatitude()
+                                )).collect(Collectors.toList())
+                ).reduce((prev, cur) -> {
+                    prev.addAll(cur);
+                    return prev;
+                });
+        if (result.isEmpty()) throw new BaseException(BaseResponseStatus.NOT_EXIST_LANDMARK);
+        return result.get();
     }
-}
+}g
