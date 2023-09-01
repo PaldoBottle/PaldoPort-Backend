48b4ca876d1cbba2bb6796f2317fff28f86d29f5
shortboy7 <thdwldnd7@naver.com>
2023-09-01 15:43:32 +0900

    feat:add get stamp list controller

--- src/main/java/paldo_bottle/backend/stamp/controller/StampUserController.java
+++ src/main/java/paldo_bottle/backend/stamp/controller/StampUserController.java
@@ -5,10 +5,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import paldo_bottle.backend.DTO.GetStampListReq;
+import paldo_bottle.backend.DTO.GetStampListResItem;
 import paldo_bottle.backend.DTO.PublishStampDtoReq;
 import paldo_bottle.backend.DTO.PublishStampDtoRes;
 import paldo_bottle.backend.global.exception.BaseException;
 import paldo_bottle.backend.stamp.service.StampService;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,6 +26,16 @@ public class StampUserController {
             return new ResponseEntity<>(publishStampDtoRes, HttpStatus.OK);
         } catch (BaseException exception) {
             return new ResponseEntity<>(exception.getMessage(), exception.getCode());
+        }
+    }
+
+    @GetMapping("/stamp/user/list")
+    public ResponseEntity getStampList(String userId) {
+        try{
+            List<GetStampListResItem> stampList = this.stampService.getStampList(userId, new GetStampListReq());
+            return new ResponseEntity(stampList, HttpStatus.OK);
+        } catch (BaseException e) {
+            return new ResponseEntity(e.getMessage(), e.getCode());
         }
     }
 }
