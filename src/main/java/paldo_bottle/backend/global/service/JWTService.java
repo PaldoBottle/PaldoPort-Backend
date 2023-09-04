37000c690f9c01f111d403767ecb80329088a722
pletain <pletain@dgu.ac.kr>
2023-09-04 21:14:57 +0900

    feat: create jwt service

---
+++ src/main/java/paldo_bottle/backend/global/service/JWTService.java
@@ -0,0 +1,68 @@
+package paldo_bottle.backend.global.service;
+
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Service
+public class JWTService {
+    @Value("${PALDOJWT.ISSUER_SECRET}")
+    private String Issuer;
+
+    //JWT 생성
+    public String makeJwtToken(String userId) {
+        Date now = new Date();
+        return Jwts.builder()
+                .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
+                .setIssuer(Issuer)
+                .setIssuedAt(now)
+                .setExpiration(new Date(now.getTime() + Duration.ofMinutes(30).toMillis()))
+                .claim("id", userId)
+                .signWith(SignatureAlgorithm.HS256, "secret")
+                .compact();
+    }
+
+    //JWT 토큰 파싱 및 유효성 확인
+    public String doFilterInternal(String authorizationHeader) throws IOException, ServletException {
+        Claims claims = parseJwtToken(authorizationHeader);
+        return getUserIdFromJWT(claims);
+    }
+
+    protected Claims parseJwtToken(String authorizationHeader) {
+        validationAuthorizationHeader(authorizationHeader);
+        String token = extractToken(authorizationHeader);
+
+        return Jwts.parser()
+                .setSigningKey("secret")
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    protected static String getUserIdFromJWT(Claims claims) {
+        return (String) claims.get("id");
+    }
+
+    private String extractToken(String authorizationHeader) {
+        return authorizationHeader.substring("Bearer ".length());
+    }
+
+    private void validationAuthorizationHeader(String header) {
+        if (header == null || !header.startsWith("Bearer ")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    public JWTService() {}
+}
