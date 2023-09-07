048074e14aeaa3c092ca50d4aff6215e78f84ba9
Jiwoong Song <55657581+shortboy7@users.noreply.github.com>
2023-09-07 15:23:29 +0900

    Revert "Stamp load"

--- src/main/java/paldo_bottle/backend/global/MockDataLoader.java
+++ src/main/java/paldo_bottle/backend/global/MockDataLoader.java
@@ -65,30 +65,6 @@ public class MockDataLoader implements ApplicationRunner {
         buyeo.addLandmark(buyeo_landmark);
         regions.add(buyeo);
 
-        Region changyeong = create_region_stamp("경상남도", "창녕군", "창녕입니다.", 15L,
-                "https://stampimage.s3.ap-northeast-2.amazonaws.com/Gyeongsangnam-do_Changnyeong-gun.png");
-        Landmark changyeong_landmark = new Landmark("화왕산성", "임진왜란 때 군사적 요충지였습니다.", 128.5374, 35.5458);
-        changyeong.addLandmark(changyeong_landmark);
-        regions.add(changyeong);
-
-        Region namwon = create_region_stamp("전라북도", "남원시", "남원시입니다.", 15L,
-                "https://stampimage.s3.ap-northeast-2.amazonaws.com/Jeollabuk-do_Namwon-si.png");
-        Landmark namwon_landmark = new Landmark("광한루", "춘향전의 배경입니다", 127.3794, 35.4038);
-        namwon.addLandmark(namwon_landmark);
-        regions.add(namwon);
-
-        Region wando = create_region_stamp("전라남도", "완도군", "완도군입니다.", 15L,
-                "https://stampimage.s3.ap-northeast-2.amazonaws.com/Jeollanam-do_Wando-gun.png");
-        Landmark wando_landmark = new Landmark("청해진 유적지", "청해진 유적지입니다.", 126.7352, 34.3586);
-        wando.addLandmark(wando_landmark);
-        regions.add(wando);
-
-        Region cheorwon = create_region_stamp("강원도", "철원군", "철원군입니다.", 15L,
-                "https://stampimage.s3.ap-northeast-2.amazonaws.com/Gangwon-do_Cheorwon-gun.png");
-        Landmark cheorwon_landmark = new Landmark("철원 노동당사", "북한이 건설했던 노동당사입니다.", 127.2019, 38.2551);
-        cheorwon.addLandmark(cheorwon_landmark);
-        regions.add(cheorwon);
-
         regionRepository.saveAll(regions);
     }
 
@@ -97,11 +73,7 @@ public class MockDataLoader implements ApplicationRunner {
 
         Stamp stamp1 = stampRepository.findById(new RegionID("충청남도", "제천시")).get();
         Stamp stamp2 = stampRepository.findById(new RegionID("충청북도", "부여군")).get();
-        Stamp se_jong = stampRepository.findById(new RegionID("서울시", "중구")).get();
-        Stamp jn_wando = stampRepository.findById(new RegionID("전라남도", "완도군")).get();
-        Stamp gang_cheorwon = stampRepository.findById(new RegionID("강원도", "철원군")).get();
-        Stamp jb_namwon = stampRepository.findById(new RegionID("전라북도", "남원시")).get();
-        Stamp gn_changyeong = stampRepository.findById(new RegionID("경상남도", "창녕군")).get();
+        Stamp stamp3 = stampRepository.findById(new RegionID("서울시", "중구")).get();
 
         Challenge chungcheongMaster = new Challenge("충청도 마스터", "충청남도 제천시와 충청북도 부여군 방문 시 획득 가능", 100L);
         StampChallenge stampChallenge1 = StampChallenge.createStampChallenge(chungcheongMaster, stamp1);
@@ -113,35 +85,11 @@ public class MockDataLoader implements ApplicationRunner {
 
         Challenge jonggubuyeoMaster = new Challenge("중구 부여 마스터", "서울특별시 중구와 충청북도 부여 방문 시 획득 가능", 150L);
         StampChallenge stampChallenge3 = StampChallenge.createStampChallenge(jonggubuyeoMaster, stamp2);
-        StampChallenge stampChallenge4 = StampChallenge.createStampChallenge(jonggubuyeoMaster, se_jong);
+        StampChallenge stampChallenge4 = StampChallenge.createStampChallenge(jonggubuyeoMaster, stamp3);
 
         jonggubuyeoMaster.add(stampChallenge3);
         jonggubuyeoMaster.add(stampChallenge4);
         challenges.add(jonggubuyeoMaster);
-
-        challenges.add(
-                make_challenge(
-                        "전라도 마스터",
-                        "전라도 전 지역 방문시 획득",
-                        150L,
-                        jb_namwon,
-                        jb_namwon)
-        );
-        challenges.add(
-                make_challenge("서울시 마스터",
-                        "서울시 전 지역 방문시 획득",
-                        150L,
-                        se_jong)
-        );
-        challenges.add(
-                make_challenge(
-                        "분단의 아픔",
-                        "6.25 관련 시설 전 지역 방문시 획득",
-                        150L,
-                        gang_cheorwon
-                )
-        );
-
 
         challengeRepository.saveAll(challenges);
     }
@@ -157,14 +105,5 @@ public class MockDataLoader implements ApplicationRunner {
         stamp.setRegion(region);
         region.setStamp(stamp);
         return region;
-    }
-
-    private Challenge make_challenge(String challenge_name, String challenge_description, Long challenge_point,
-                                    Stamp ... stamps) {
-        Challenge challenge = new Challenge(challenge_name, challenge_description, challenge_point);
-        for (Stamp stamp : stamps) {
-            challenge.add(StampChallenge.createStampChallenge(challenge, stamp));
-        }
-        return challenge;
     }
 }
